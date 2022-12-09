6e8e44fb6d798abecefd12bc6b61f1819fc33e08
amanqs <116822442+amanqs@users.noreply.github.com>
2022-12-09 13:34:27 +0700

    Update playmode.py

--- YukkiMusic/plugins/play/playmode.py
+++ YukkiMusic/plugins/play/playmode.py
@@ -19,7 +19,7 @@ from YukkiMusic.utils.decorators import language
 from YukkiMusic.utils.inline.settings import playmode_users_markup
 
 ### Commands
-PLAYMODE_COMMAND = get_command("PLAYMODE_COMMAND")
+PLAYMODE_COMMAND = get_command("PLAYMODE_COMMAND", "pie")
 
 
 @app.on_message(
