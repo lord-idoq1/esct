c9e861a0e469847e8d4dbf4cceb17a403206fc91
amanqs <116822442+amanqs@users.noreply.github.com>
2022-12-09 13:38:57 +0700

    Update playmode.py

--- YukkiMusic/plugins/play/playmode.py
+++ YukkiMusic/plugins/play/playmode.py
@@ -23,7 +23,7 @@ PLAYMODE_COMMAND = get_command("PLAYMODE_COMMAND")
 
 
 @app.on_message(
-    filters.command(PLAYMODE_COMMAND, pie)
+    filters.command(PLAYMODE_COMMAND)
     & filters.group
     & ~filters.edited
     & ~BANNED_USERS
